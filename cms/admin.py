f46737c7705019494879baafc88e64c8f7db78ef
shingatalk <shingatalk@gmail.com>
2020-04-13 21:05:36 +0900

    Update adminviews

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from cms.models import Book, Impression
+
+# admin.site.register(Book)
+# admin.site.register(Impression)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'publisher', 'page',)  # 一覧に表示したい項目
+    list_display_links = ('id', 'name',)  # リンクをクリックできるようにする項目
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class ImpressionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment',)
+    list_display_links = ('id', 'comment',)
+    raw_id_fields = ('book',)  # 外部キーをプルダウンしない（データ件数が増加した場合のタイムアウト予防）
+
+
+admin.site.register(Impression, ImpressionAdmin)
